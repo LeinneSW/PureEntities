37c6333770f392dcfbbfaf328ebe291af94a2cbc
LeinneSW <nno88551@naver.com>
2019-09-17 16:02:33 +0900

    점프 체크 구조 변경

--- src/leinne/pureentities/entity/ai/EntityAI.php
+++ src/leinne/pureentities/entity/ai/EntityAI.php
@@ -8,38 +8,43 @@ use pocketmine\block\Stair;
 use pocketmine\entity\Entity;
 use pocketmine\math\Facing;
 use pocketmine\math\Vector3;
+use pocketmine\world\World;
+use pocketmine\math\AxisAlignedBB;
 
 class EntityAI{
 
-    const JUMP_CANT = 0;
-    const JUMP_BLOCK = 1;
-    const JUMP_SLAB = 2;
-    const JUMP_STAIR = 3;
+    const WALL = 0;
+    const BLOCK = 1;
+    const SLAB = 2;
+    const STAIR = 3;
 
-    public static function checkJumpState(Entity $entity) : int{
-        $motion = $entity->getMotion();
-        $x = (int) (($motion->x > 0 ? $entity->boundingBox->maxX : $entity->boundingBox->minX) + $motion->x);
-        $z = (int) (($motion->z > 0 ? $entity->boundingBox->maxZ : $entity->boundingBox->minZ) + $motion->z);
-
-        $block = $entity->getWorld()->getBlock(new Vector3($x, $entity->getLocation()->getY(), $z));
+    /**
+     * @var AxisAlignedBB|Entity $entity
+     */
+    public static function checkBlockState(World $world, AxisAlignedBB $aabb, Vector3 $motion) : int{
+        $block = $world->getBlock(new Vector3(
+            (int) (($motion->x > 0 ? $aabb->maxX : $aabb->minX) + $motion->x),
+            (int) $aabb->minY,
+            (int) (($motion->z > 0 ? $aabb->maxZ : $aabb->minZ) + $motion->z)
+        ));
         if(!isset($block->getCollisionBoxes()[0]) || !isset($block->getSide(Facing::UP, 2)->getCollisionBoxes()[0])){
-            return EntityAI::JUMP_CANT;
+            return EntityAI::WALL;
         }
 
-        $aabb = $block->getCollisionBoxes()[0];
+        $blockBox = $block->getCollisionBoxes()[0];
         if(($up = $block->getSide(Facing::UP)->getCollisionBoxes()[0]) === \null){ /** 위에 아무 블럭이 없을 때 */
-            if($aabb->maxY - $aabb->minY > 1 || $aabb->maxY === $entity->getLocation()->getY()){ /** 울타리 or 반블럭 위 */
-                return EntityAI::JUMP_CANT;
+            if($blockBox->maxY - $blockBox->minY > 1 || $blockBox->maxY === $aabb->minY){ /** 울타리 or 반블럭 위 */
+                return EntityAI::WALL;
             }else{
                 if($block instanceof Stair){
-                    return EntityAI::JUMP_STAIR;
+                    return EntityAI::STAIR;
                 }
-                return $aabb->maxY - $entity->getLocation()->getY() === 0.5 ? EntityAI::JUMP_SLAB : EntityAI::JUMP_BLOCK;
+                return $blockBox->maxY - $aabb->minY === 0.5 ? EntityAI::JUMP_SLAB : EntityAI::JUMP_BLOCK;
             }
-        }elseif($up->maxY - $entity->getLocation()->getY() === 1.0){ /** 반블럭 위에서 반블럭 * 3 점프 */
-            return EntityAI::JUMP_BLOCK;
+        }elseif($up->maxY - $aabb->minY === 1.0){ /** 반블럭 위에서 반블럭 * 3 점프 */
+            return EntityAI::BLOCK;
         }
-        return EntityAI::JUMP_CANT;
+        return EntityAI::WALL;
     }
 
 }
