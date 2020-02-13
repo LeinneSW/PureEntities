82db30ad413e1cd95465cce9d99735a8f66d1f6b
Leinne <nno88551@naver.com>
2020-02-13 16:54:25 +0900

    약간의 코드 개선

--- src/leinne/pureentities/block/MonsterSpawner.php
+++ src/leinne/pureentities/block/MonsterSpawner.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace leinne\pureentities\block;
 
+use leinne\pureentities\tile\MonsterSpawner as TileSpawner;
+
 use pocketmine\block\MonsterSpawner as PMMonsterSpawner;
 use pocketmine\entity\EntityFactory;
 use pocketmine\player\Player;
@@ -12,7 +14,7 @@ class MonsterSpawner extends PMMonsterSpawner{
 
     public function onScheduledUpdate(): void{
         $spawner = $this->getPos()->getWorld()->getTile($this->getPos());
-        if(!$spawner instanceof \leinne\pureentities\tile\MonsterSpawner || $spawner->closed){
+        if(!$spawner instanceof TileSpawner || $spawner->closed){
             return;
         }
         if(!$spawner->hasValidEntityId()){
@@ -41,7 +43,7 @@ class MonsterSpawner extends PMMonsterSpawner{
                 $pos = $spawner->getPos()->asPosition();
                 $pos->x += mt_rand(0, 1) ? $newx : -$newx;
                 $pos->z += mt_rand(0, 1) ? $newz : -$newz;
-                //$pos->y = $this->calculateYPos($pos->x, $pos->z);
+                $pos->y = $this->calculateYPos($pos);
                 $nbt = EntityFactory::createBaseNBT($pos);
                 $nbt->setString("identifier", $spawner->getEntityId());
                 $entity = EntityFactory::createFromData($spawner->getPos()->getWorld(), $nbt);
@@ -52,9 +54,9 @@ class MonsterSpawner extends PMMonsterSpawner{
         }
     }
 
-    public function calculateYPos(int $x, int $y, int $z) : int{
+    public function calculateYPos(Position $pos) : int{
         //TODO: 몬스터가 박히지 않을 최소한의 Y좌표
-        return 0;
+        return $pos->y;
     }
 
 }
